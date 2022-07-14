de28be6b4ed158c92e98212a7caed7187f7553f8
Harsh Valaki <96899883+hxrsh7@users.noreply.github.com>
2022-07-14 23:51:29 +0530

    Prototype completed

---
+++ Assets/Scripts/BallRotation.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallRotation : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    private void FixedUpdate()
+    {
+        transform.Rotate(0, 0, speed * Time.deltaTime);
+    }
+
+    public void ChangeDirection()
+    {
+        speed = -speed;
+    }
+}
