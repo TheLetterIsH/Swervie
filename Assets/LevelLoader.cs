534cde7be45465e007b790fab03f92e50322e4cf
Harsh Valaki <96899883+hxrsh7@users.noreply.github.com>
2022-07-17 15:51:49 +0530

    Animation failed

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+
+    public float trnsTime = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+          if (Input.GetMouseButtonDown(0))
+        {
+            LoadNextLevel();
+        }
+    }
+    
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        // Play animation
+        transition.SetTrigger("start");
+
+        // Wait
+        yield return new WaitForSeconds(trnsTime);
+
+        // Load scene
+        SceneManager.LoadScene(levelIndex);
+    }
+}
